4bc00755b2ffa7d52cf5c4dda8a0937963f7bf1c
Smokie Lee <xtoq@users.noreply.github.com>
2015-08-11 12:20:58 -0500

    First time committing a git, as well as doing it in conjunction with NetBeans...here goes!

---
+++ Shout Box/src/shout/box/ShoutBox.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shout.box;
+
+/**
+ *
+ * @author Smokie Lee
+ */
+public class ShoutBox {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
