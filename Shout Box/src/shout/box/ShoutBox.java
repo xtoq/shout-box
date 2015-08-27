bb67221b739dcf144f2368deebe1b177d63e089a
Smokie Lee <xtoq@users.noreply.github.com>
2015-08-26 21:33:14 -0500

    0.3
    - created createMessageList class
    - update createMessageList constructor to include arguments to create
    the hash map
    - deleted commented out code
    - created getCount to get user input for message list size

--- Shout Box/src/shout/box/ShoutBox.java
+++ Shout Box/src/shout/box/ShoutBox.java
@@ -6,107 +6,42 @@
 package shout.box;
 
 // NOTE: imports
-import java.util.*; // Since we need 3 imports, this seems more prudent. Also endorsed by the Java documentation.
+import java.util.HashMap; // Since we need 3 imports, this seems more prudent. Also endorsed by the Java documentation.
 
 /**
+ * The ShoutBox class collects and displays user messages.
  *
  * @author Smokie Lee
  */
 public class ShoutBox {
-  private final userInput u = new userInput(); // Create new user input object.
-//  private final createMessageList l = new createMessageList(); // Create new message list object.
-  
-  // NOTE: getters
-  
-  /**
-   * Print out each message in the array list in a numbered list.
-   * @param messageListName Name of the message list.
-   * @return getMessages    Returns a numbered list of messages.
-   */
-  public String getMessages(HashMap<Integer, String> messageListName) {
-    String getMessages = "No messages entered.";
-    for(Map.Entry<Integer, String> entry : messageListName.entrySet()) { // Enahnced for loop is more efficient here when we're iterating through the set.
-       System.out.printf("%s: %s\n",entry.getKey(),entry.getValue()); // Print key and value as a list.
-       // TODO: format this with tabs for extra goodness
-    }
-    return getMessages;
-  }
-  
-  /**
-   *
-   * @param messageListName Name of array list to access.
-   * @param messageNumber   Array index number.
-   * @return message        Returns message at provided index value.
-   */
-//  public String getSingleMessage(int messageNumber,ArrayList<String> messageListName) {
-  public String getSingleMessage(int messageNumber,HashMap<Integer, String> messageListName) {
-    String message = ""; // Initialize variable for later use.
-    System.out.printf("%s%n",messageListName.get(messageNumber));
-    return message; // Return selected message.
-  }
-  
-//  public String shoutOutCannedMessage(ArrayList<String> messageListName) {
-  public String shoutOutCannedMessage(HashMap<Integer, String> messageListName) {
-    int messageNumber = 0; // Initalize variable for holding the user input.
-    do {
-      System.out.printf("%nPlease type a number from the list below to print that message.%n%n");
-      getMessages(messageListName); // Print messages from the list in args.
-      System.out.printf("%n>> ");
-      messageNumber = u.getUserInteger(); // Set user input to the index value. 
-    }
-    while (messageNumber > messageListName.size()); // Keep iterating if user enters something outside the array index.
-
-    System.out.printf("%n");
-
-    return getSingleMessage(messageNumber, messageListName); // Print selected message.
-  }
-  
-  // NOTE: setters
-  
-  /**
-   * Collect messages from the user.
-   * @param numMessages     Number of messages to enter.
-   * @param messageListName Name of the array message list to add to.
-   */    
-//  public void setMessages(int numMessages,ArrayList<String> messageListName) {
-  public void setMessages(Integer numMessages, HashMap<Integer, String> messageListName) {
-    for (int i = 1; i <= numMessages; i++) { // Start the count at 1 instead of 0.
-      System.out.printf("%nPlease enter a message:%n>> ");
-      messageListName.put(i, u.getUserString()); // Add user input into hash map.
-    }
-  }
 
   /**
    * @param args the command line arguments
    */
-//  @SuppressWarnings("unchecked")
   public static void main(String[] args) {
-    
+
     // Create objects.
-    ShoutBox a = new ShoutBox(); // Create main ShoutBox object.
-    userInput u = new userInput(); // Create new user input object.
-    HashMap<Integer, String> messageList = new HashMap<>(); // Create array list to hold user messages.
-    Integer count; // Create a variable to hold the number of messages the user wants to input.
-    
+    userInput input = new userInput(); // Create new user input object.
+    createMessageList list = new createMessageList();
+    HashMap<Integer, String> messageMap = new HashMap<>(); // Create hash map to hold user messages.
+
     // Welcome the user.
     System.out.printf("Welcome to the \"ShoutBox\" program. Let's shout some messages!%n%n");
-    
+
     // Collect number of messages from user (this is the size of the hash map).
     System.out.printf("How many messages would you like to enter? Please enter a number.%n>> ");
-    count = u.getUserInteger(); // Set count variable to user input.
-    
-    // Collect the messages from the user (these are the array list values).
-    a.setMessages(count, messageList);
-    
-    // Print messages.
-    a.shoutOutCannedMessage(messageList);
+//    Integer count = input.getUserInteger(); // Create and set count variable to user input.
+//    list.getNumber();
 
-//    userInput error = new userInput(); // Test that the "error" message works.
+    // Collect the messages from the user (these are the hash map values).
+//    createMessageList messages = new createMessageList(list.getNumber(), messageMap); // Create message list object and collect user input.
+    list.setMessages(list.getNumber(), messageMap); // Create message list object and collect user input.
     
-//    System.out.printf("How many messages would you like to enter? Please enter a number.%n>> ");
-//    count = u.getUserInt();
-//    System.out.println("You entered: " + count);
-    
+
+    // Print messages.
+//    messages.shoutOutCannedMessage(messageMap);
+    list.shoutOutCannedMessage(messageMap);
+
   }
 
 }
--- Shout Box/src/shout/box/createMessageList.java
+++ Shout Box/src/shout/box/createMessageList.java
@@ -5,10 +5,97 @@
  */
 package shout.box;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Smokie Lee
  */
-public class createMessageList {
-  
+public final class createMessageList {
+
+  private final userInput u = new userInput(); // Create new user input object.
+  private Integer count;
+  private HashMap<Integer, String> list;
+
+  /**
+   * No argument constructor.
+   */
+  public createMessageList() {
+  }
+
+  /**
+   * Collect messages from user input and store them in a hash map.
+   *
+   * @param numMessages Number of messages the user wants to store.
+   * @param messageListName Name of the message list.
+   */
+  public createMessageList(Integer numMessages, HashMap<Integer, String> messageListName) {
+    // Set user input as messages.
+    setMessages(numMessages, messageListName);
+  }
+
+  /**
+   * Print out each message in the array list in a numbered list.
+   *
+   * @param messageListName Name of the message list.
+   * @return printMessageList Returns a numbered list of messages.
+   */
+  public String printMessageList(HashMap<Integer, String> messageListName) {
+    String printMessageList = "No messages entered.";
+    for (Map.Entry<Integer, String> entry : messageListName.entrySet()) { // Enahnced for loop is more efficient here when we're iterating through the set.
+      System.out.printf("%s: %s\n", entry.getKey(), entry.getValue()); // Print key and value as a list.
+      // TODO: format this with tabs for extra goodness
+    }
+    return printMessageList;
+  }
+
+  /**
+   *
+   * @param messageListName Name of array list to access.
+   * @param messageNumber Array index number.
+   * @return message Returns message at provided index value.
+   */
+  public String printMessage(int messageNumber, HashMap<Integer, String> messageListName) {
+    String message = ""; // Initialize variable for later use.
+    System.out.printf("%s%n", messageListName.get(messageNumber));
+    return message; // Return selected message.
+  }
+
+  /**
+   *
+   * @param messageListName Name of array list to access.
+   * @return message Returns a single message via the printMessage function.
+   */
+  public String shoutOutCannedMessage(HashMap<Integer, String> messageListName) {
+    int messageNumber = 0; // Initalize variable for holding the user input.
+    do {
+      System.out.printf("%nPlease type a number from the list below to print that message.%n%n");
+      printMessageList(messageListName); // Print messages from the list in args.
+      System.out.printf("%n>> ");
+      messageNumber = u.getUserInteger(); // Set user input to the index value. 
+    } while (messageNumber > messageListName.size()); // Keep iterating if user enters something outside the array index.
+
+    System.out.printf("%n");
+
+    return printMessage(messageNumber, messageListName); // Print selected message.
+  }
+
+  /**
+   * Collect messages from the user.
+   *
+   * @param numMessages Number of messages to enter.
+   * @param messageListName Name of the array message list to add to.
+   */
+  public void setMessages(Integer numMessages, HashMap<Integer, String> messageListName) {
+    for (int i = 1; i <= numMessages; i++) { // Start the count at 1 instead of 0.
+      System.out.printf("%nPlease enter a message:%n>> ");
+      messageListName.put(i, u.getUserString()); // Add user input into hash map.
+    }
+  }
+    
+  public Integer getNumber() {
+    this.count = u.getUserInteger();
+    return count;
+  }
 }
